46056fc30a9efe7a4d997356f246dde709a8b217
nomanlike <57402905+nomanlike@users.noreply.github.com>
2021-08-25 16:23:27 +0700

    making bot a bit smarter

--- 3Dgameproject/Assets/Chuong's scripts/movement.cs
+++ 3Dgameproject/Assets/Chuong's scripts/movement.cs
@@ -12,29 +12,40 @@ public class movement : MonoBehaviour
     NavMeshAgent agent;
     public GameObject ringed = null;
     GameObject destiny = null;
+    List<bool> trust=new List<bool>();
     private void Start()
     {
         
         mark = mark_for_death.instance.target.transform;
         agent = GetComponent<NavMeshAgent>();
-        
+        trust.Add(true);
     }
 
     private void Update()
     {
         float distance = Vector3.Distance(mark.position, transform.position);
-
+        int k;
         if (distance <= radius_sense)
         {
+            agent.isStopped=true;
+            agent.isStopped = false;
             agent.SetDestination(mark.position);
         }
         else
         {
+            if (ringed != null)
+            {
+                k = Random.Range(0, trust.Count - 1);
+                if (trust[k])
+                    destiny = ringed;
+                ringed = null;
+            }
+
             if (destiny == null)
                 roaming();
             else
             {
-
+               
                 agent.SetDestination(destiny.transform.position);
                 destiny = null;
             }
@@ -47,7 +58,7 @@ public class movement : MonoBehaviour
         trap[] bear;
         int i;
         bear = GameObject.FindObjectsOfType<trap>();
-        i = bear.Length;
+        i = Random.Range(0, bear.Length - 1);
         Debug.Log(i);
 
     }
