4ff7cc49239f7c35502c76a0d4c4d2e040c2b0b1
nomanlike <57402905+nomanlike@users.noreply.github.com>
2021-09-14 19:44:08 +0700

    ???

--- 3Dgameproject/Assets/Chuong's scripts/advanced AI script/Machine_brain.cs
+++ 3Dgameproject/Assets/Chuong's scripts/advanced AI script/Machine_brain.cs
@@ -7,9 +7,12 @@ using Unity.MLAgents.Actuators;
 
 public class Machine_brain : Agent
 {
-    
+    public float movespedw =1f;
+    public float movespedr= 2f;
+
     public SensorComponent eye;
-    
+    public SensorComponent heart;
+
     public override void CollectObservations(VectorSensor sensor)
     {
        
@@ -19,7 +22,19 @@ public class Machine_brain : Agent
     public override void OnActionReceived(ActionBuffers actions)
     {
         float movex = actions.ContinuousActions[0];
-        float movey = actions.ContinuousActions[1];
+        float movez = actions.ContinuousActions[1];
+      
+
+        transform.position += new Vector3(movex, 0, movez) * Time.deltaTime * movespedw;
+
     }
-    
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, movespedw);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, movespedr);
+
+    }
 }
