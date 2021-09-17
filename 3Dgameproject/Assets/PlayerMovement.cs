5ff5ae4c6a45a7a1f94f4db315943b0e1f7a0624
nomanlike <57402905+nomanlike@users.noreply.github.com>
2021-09-17 19:25:10 +0700

    ss

--- 3Dgameproject/Assets/PlayerMovement.cs
+++ 3Dgameproject/Assets/PlayerMovement.cs
@@ -5,81 +5,40 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private float walkSpeed;
-    [SerializeField] private float runSpeed;
-    [SerializeField] private bool IsGrounded;
-    [SerializeField] private LayerMask groundMask;
-    [SerializeField] private float gravity;
+    public CharacterController controller;
+    public float speed = 12f;
+    public float gravity = -9.81f;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
+    public LayerMask groundMask;
 
-    private Vector3 movedir;
-    private Vector3 velocity;
-
-    private CharacterController controller;
-
-    public Animator animator;
-   
+    Vector3 velocity;
+    bool isGrounded;
 
     void Start()
     {
-        controller = GetComponent<CharacterController>();
-        animator = GetComponentInChildren<Animator>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        Move();
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-    }
-    private void Move()
-    {
-        IsGrounded = Physics.CheckSphere(transform.position, groundDistance, groundMask);
-        if(IsGrounded && velocity.y <0)
+        if(isGrounded && velocity.y <0)
         {
             velocity.y = -2f;
         }
-        float moveZ = Input.GetAxis("Vertical");
-        movedir = new Vector3(0, 0, moveZ);
-        movedir = transform.TransformDirection(movedir);
-        
-        if(movedir != Vector3.zero && !Input.GetKey(KeyCode.LeftShift))
-        {
-            Walk();
-        }
-        else if (movedir != Vector3.zero && Input.GetKey(KeyCode.LeftShift))
-        {
-            Run();
-        }
-        else if (movedir == Vector3.zero)
-        {
-            Idle();
-        }
-        movedir *= moveSpeed;
 
-        controller.Move(movedir * Time.deltaTime);
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
 
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
 
     }
-    private void Idle()
-    {
-        animator.SetFloat("speed", 0);
-    }
-    private void Walk()
-    {
-        moveSpeed = walkSpeed;
-        animator.SetFloat("speed", 0.5f);
-    }
-    private void Run()
-    {
-        moveSpeed = runSpeed;
-        animator.SetFloat("speed", 1f);
-    }
-
 }
