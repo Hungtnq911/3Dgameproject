4a5c0afb57a01fa7972b21429200a284ce3ee9bb
inlovewithPC <trannguyenquochung00@gmail.com>
2021-09-20 10:09:00 +0700

    update bot nav

--- 3Dgameproject/Assets/Patrol.cs
+++ 3Dgameproject/Assets/Patrol.cs
@@ -58,7 +58,7 @@ public class Patrol : MonoBehaviour
             animator.SetFloat("botSpeed", 1f);
         }
         else
-        if (!agent.pathPending && agent.remainingDistance < 20f)
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
             GotoNextPoint();
 
     }
